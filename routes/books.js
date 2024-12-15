611aded208503e6f2711a538d1349b34c2c2807b
lucky9457 <luckyreddychintu@gmail.com>
2024-12-15 10:52:29 +0530

    mainu

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const QRCode = require('qrcode');
-const Book = require('../models/Book');
+const Book = require('../models/book');
 const authenticate = require('../middleware/authenticate');
 
 const router = express.Router();
